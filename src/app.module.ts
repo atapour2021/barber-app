a45ec71939fc6d74e8679968351baa0a968d3f5b
kianoush atapour <atapour2010@gmail.com>
2026-06-19 09:56:05 -0700

    implement project architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { BarbershopsModule } from './modules/barbershops/barbershops.module';
+import { BarbersModule } from './modules/barbers/barbers.module';
+import { ServicesModule } from './modules/services/services.module';
+import { AppointmentsModule } from './modules/appointments/appointments.module';
+import { EducationalModule } from './modules/educational/educational.module';
+import { CertificatesModule } from './modules/certificates/certificates.module';
+import { UploadsModule } from './modules/uploads/uploads.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UsersModule, BarbershopsModule, BarbersModule, ServicesModule, AppointmentsModule, EducationalModule, CertificatesModule, UploadsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/modules/index.ts
@@ -0,0 +1,10 @@
+export * from './appointments/appointments.module';
+export * from './auth/auth.module';
+export * from './barbers/barbers.module';
+export * from './barbershops/barbershops.module';
+export * from './certificates/certificates.module';
+export * from './educational/educational.module';
+export * from './services/services.module';
+export * from './uploads/uploads.module';
+export * from './users/users.module';
+
